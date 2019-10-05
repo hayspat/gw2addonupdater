913abe290991328644f3b0ae4be0328f360305eb
Faruk Aydın <hayspat@gmail.com>
2019-10-05 11:49:42 +0300

    Beta version

--- classupdater.py
+++ classupdater.py
@@ -1,20 +1,49 @@
 from urllib.request import urlopen
 from pathlib import Path
-import json, wget, os, shutil
-import arcdps
+import json, wget, os, shutil, platform, configparser, requests
 
+def createconfig():
+    # Create the configuration file as it doesn't exist yet
+    cfgfile = open(inipath, 'w')
+    # Add content to the file
+    Config = configparser.ConfigParser()
+    Config.add_section('arcdps')
+    Config.set('arcdps', 'version', '0')
+    Config.set('arcdps', 'status', 'disabled')
+    Config.add_section('gw2radial')
+    Config.set('gw2radial', 'version', '0')
+    Config.set('gw2radial', 'status', 'disabled')
+    Config.add_section('d9vk')
+    Config.set('d9vk', 'version', '0')
+    Config.set('d9vk', 'status', 'disabled')
+    Config.add_section('path')
+    Config.set('path', 'gw2path', "None")
+    Config.write(cfgfile)
+    cfgfile.close()
 
+def arcgetversion():
+    content = requests.get('https://www.deltaconnected.com/arcdps/x64/d3d9.dll.md5sum')
+    try:
+        content.raise_for_status()
+    except Exception as exc:
+        print('Bir hata oluştu: %s' % (exc))
+    md5 = content.text[:32]
+    return md5
+    
 class AddonUpdater:
     def __init__(self, name, url, version="0", status = "disabled"):
-        self.name = name
+        self.name = name 
         self.url = url
-        self.version = version
-        self.status = status
+        self.version = parser.get(self.name, 'version')
+        self.status = parser.get(self.name, 'status')
         if self.name != "arcdps":
             version = self.getversion()
             self.latestversion = version[0]
             self.downloadurl = version[1]
             self.zipname = version[2]
+        else:
+            self.latestversion = arcgetversion()
+            self.downloadurl = self.url
         
     def getversion(self):  
         with urlopen (self.url) as response:
@@ -30,10 +59,19 @@ class AddonUpdater:
         if os.path.exists('./.tmp') == False:
             os.mkdir('./.tmp')
         os.chdir(os.path.join(currentdir, ".tmp"))
-        wget.download(self.downloadurl)
-        filename = shutil.unpack_archive(self.zipname, os.path.join(currentdir, ".tmp", self.name))
-        os.remove(self.zipname)
-        
+        if self.name != "arcdps":
+            wget.download(self.downloadurl)
+            filename = shutil.unpack_archive(self.zipname, os.path.join(currentdir, ".tmp", self.name))
+            os.remove(self.zipname)
+        if self.name == "arcdps":
+            if os.path.exists('./arcdps') == False:
+                os.mkdir('./arcdps')
+            os.chdir(os.path.join(currentdir, ".tmp", "arcdps"))
+            try:
+                os.remove("./d3d9.dll")
+            except:
+                wget.download(self.downloadurl)
+
     def setname():
         if (d9vk.status == "enabled") and (arcdps.status == "enabled") and (gw2radial.status == "enabled"):
             arcdps.fn = "d3d9.dll"
@@ -51,17 +89,62 @@ class AddonUpdater:
             arcdps.fn = "d3d9.dll"
             gw2radial.fn = "d3d9_chainload.dll"
             d9vk.fn = "none"
-        else:
-            gw2radial.fn = "d3d9.dll"
-            arcdps.fn = "d3d9.dll"
+        elif (arcdps.status == "disabled" and gw2radial.status == "disabled") and d9vk.status == "enabled":
+            gw2radial.fn = "none"
+            arcdps.fn = "none"
             d9vk.fn = "d3d9.dll"
+        elif (arcdps.status == "disabled" and d9vk.status == "disabled") and gw2radial.status == "enabled":
+            gw2radial.fn = "d3d9.dll"
+            arcdps.fn = "none"
+            d9vk.fn = "none"
+        elif (d9vk.status == "disabled" and gw2radial.status == "disabled") and arcdps.status == "enabled":
+            gw2radial.fn = "none"
+            arcdps.fn = "d3d9.dll"
+            d9vk.fn = "none"
+        elif d9vk.status == "disabled" and gw2radial.status == "disabled" and arcdps.status == "disabled":
+            gw2radial.fn = "none"
+            arcdps.fn = "none"
+            d9vk.fn = "none"
         
-        
-    
+    def copy(self):
+        if os.path.exists(os.path.join(parser["path"]["gw2path"], "bin64")) == False:
+            os.mkdir(os.path.join(parser["path"]["gw2path"], "bin64"))
+        bindir = os.listdir(os.path.join(parser["path"]["gw2path"], "bin64"))
+        for item in bindir:
+            if item.startswith("d3d9"):
+                os.remove(os.path.join(parser["path"]["gw2path"], "bin64", item))
+        for i in [arcdps, gw2radial, d9vk]:
+            if i.name == "d9vk":
+                dllpath = os.path.join(currentdir, ".tmp", i.name, i.name + "-" + i.latestversion, pla, "d3d9.dll")
+            if i.name == "gw2radial":
+                dllpath = os.path.join(currentdir, ".tmp", i.name, "d3d9.dll")
+            if i.name == "arcdps":
+                dllpath = os.path.join(currentdir, ".tmp", i.name, "d3d9.dll")
+            if i.fn != "none":
+                if os.path.isfile(dllpath):
+                    shutil.copy(dllpath, os.path.join(parser["path"]["gw2path"], "bin64", i.fn))
+                else:
+                    self.unpack()
+                    shutil.copy(dllpath, os.path.join(parser["path"]["gw2path"], "bin64", i.fn))
+
+                    
   
 
-currentdir = Path(__file__).parent.absolute()    
+currentdir = Path(__file__).parent.absolute()
+inipath = os.path.join(currentdir, "settings.ini")
+if not os.path.isfile(inipath):
+    createconfig()
+parser = configparser.ConfigParser()
+parser.read(inipath)
+
+
+    
+if platform.architecture()[0] == "64bit":
+    pla = "x64"
+else:
+    pla = "x32"
+    
+    
 d9vk = AddonUpdater("d9vk", "https://api.github.com/repos/Joshua-Ashton/d9vk/releases")
 gw2radial = AddonUpdater("gw2radial", "https://api.github.com/repos/Friendly0Fire/GW2Radial/releases")
 arcdps = AddonUpdater("arcdps", "https://www.deltaconnected.com/arcdps/x64/d3d9.dll")
-d9vk.unpack()
--- pygtk.py
+++ pygtk.py
@@ -2,75 +2,147 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 from classupdater import *
-from arcdps import *
-
 
 class Handler:
     def on_window_destroy(self, *args):
+        with open(inipath, 'w') as configfile:
+            parser.write(configfile)
         Gtk.main_quit()
         
     def chooser_file_set_cb(self, button):
-        print("selected: %s"%button.get_filename())
-        AddonUpdater.gw2path = button.get_filename()
-        print(AddonUpdater.gw2path)        
-    def gw2radial_button_clicked_cb(self, button):
+        gw2path = button.get_filename()
+        parser["path"]["gw2path"] = gw2path
+ 
+    def gw2radial_button_clicked_cb(self, button):      
+        gw2radial.unpack()
+        AddonUpdater.setname()
+        AddonUpdater.copy(gw2radial)
+        gw2radial.version = gw2radial.latestversion
+        parser["gw2radial"]["version"] = "{}".format(gw2radial.latestversion)
+        gw2radial_button.hide() 
+        
+    def gw2radial_switch_activate_cb(self, button):
         pass
-    def gw2radial_activate_cb(self, button):
-        pass
-    def gw2radial_state_set_cb(self, arg1, arg2):
+    def gw2radial_switch_state_set_cb(self, arg1, arg2):
         if arg2:
-            gw2radial.status = "enabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
-
+            if gw2radial.status == "enabled":
+                pass
+            else:
+                gw2radial.status = "enabled"
+                parser["gw2radial"]["status"] = "enabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(gw2radial)
+                gw2radial.version = gw2radial.latestversion
+                parser["gw2radial"]["version"] = "{}".format(gw2radial.latestversion)
         else:
-            gw2radial.status = "disabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
+            if gw2radial.status == "disabled":
+                pass
+            else:
+                gw2radial.status = "disabled"
+                parser["gw2radial"]["status"] = "disabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(gw2radial)
+                gw2radial_button.hide()                
 
-    def arcpds_button_clicked_cb (self, button):
-        pass
+    def arcpds_button_clicked_cb (self, button):   
+        arcdps.unpack()
+        AddonUpdater.setname()
+        AddonUpdater.copy(arcdps)
+        arcdps.version = arcdps.latestversion
+        parser["arcdps"]["version"] = "{}".format(arcdps.latestversion)
+        arcdps_button.hide() 
     def arcdps_switch_activate_cb(self, button):
-        print("DEneme")
+        pass
     def arcdps_switch_state_set_cb(self, arg1, arg2):
         if arg2:
-            arcdps.status = "enabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
+            if arcdps.status == "enabled":
+                pass
+            else:
+                arcdps.status = "enabled"
+                parser["arcdps"]["status"] = "enabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(arcdps)
+                arcdps.version = arcdps.latestversion
+                parser["arcdps"]["version"] = "{}".format(arcdps.latestversion)
         else:
-            arcdps.status = "disabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
-            
+            if arcdps.status == "disabled":
+                pass
+            else:
+                arcdps.status = "disabled"
+                parser["arcdps"]["status"] = "disabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(arcdps)
+                arcdps_button.hide()
 
-    def on_d9vk_button_clicked(self, button):
-        print(d9vk.getversion())
+    def on_d9vk_button_clicked(self, button):      
+        d9vk.unpack()
+        AddonUpdater.setname()
+        AddonUpdater.copy(d9vk)
+        d9vk.version = d9vk.latestversion
+        parser["d9vk"]["version"] = "{}".format(d9vk.latestversion)
+        d9vk_button.hide() 
     def d9vk_switch_activate_cb(self, button):
         pass
     def d9vk_switch_state_set_cb(self, arg1, arg2):
         if arg2:
-            d9vk.status = "enabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
-
+            if d9vk.status == "enabled":
+                pass
+            else:
+                d9vk.status = "enabled"
+                parser["d9vk"]["status"] = "enabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(d9vk)
+                d9vk.version = d9vk.latestversion
+                parser["d9vk"]["version"] = "{}".format(d9vk.latestversion)
         else:
-            d9vk.status = "disabled"
-            AddonUpdater.setname()
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.fn, arcdps.fn, d9vk.fn))
-            print("Gw2Radial: {}\nArcdps: {}\nD9VK: {}\n".format(gw2radial.status, arcdps.status, d9vk.status))
+            if d9vk.status == "disabled":
+                pass
+            else:
 
-
+                d9vk.status = "disabled"
+                parser["d9vk"]["status"] = "disabled"
+                AddonUpdater.setname()
+                AddonUpdater.copy(d9vk)
+                d9vk_button.hide()
 
 builder = Gtk.Builder()
 builder.add_from_file("final.glade")
 builder.connect_signals(Handler())
-
 window = builder.get_object("window")
 window.show_all()
 
+
+chooser = builder.get_object("chooser")
+chooser.set_filename(parser["path"]["gw2path"])
+
+        
+d9vk_button = builder.get_object("d9vk_button")
+gw2radial_button = builder.get_object("gw2radial_button")
+arcdps_button = builder.get_object("arcdps_button")
+
+arcdps_switch = builder.get_object("arcdps_switch")
+gw2radial_switch = builder.get_object("gw2radial_switch")
+d9vk_switch = builder.get_object("d9vk_switch")
+
+if gw2radial.status == "enabled":
+    gw2radial_switch.props.state = True
+if arcdps.status == "enabled":
+    arcdps_switch.props.state = True
+if d9vk.status == "enabled":
+    d9vk_switch.props.state = True
+    
+if d9vk.version == d9vk.latestversion or d9vk.status != "enabled":   
+    d9vk_button.hide()
+if arcdps.version == arcdps.latestversion or arcdps.status != "enabled":   
+    arcdps_button.hide()
+if gw2radial.version == gw2radial.latestversion or gw2radial.status != "enabled":   
+    gw2radial_button.hide()
+    
+print(d9vk.version, d9vk.latestversion)
+print(gw2radial.version, gw2radial.latestversion)
+print(arcdps.version, arcdps.latestversion)
+
+
+
+    
 Gtk.main()
